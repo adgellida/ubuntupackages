e87b6de5b20f1c17b88191a7b7838fecb79ccace
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-01-01 17:12:09 +0100

    Updating

--- gitpushubun.bat
+++ gitpushubun.bat
@@ -1,1 +1,1 @@
-C:\Windows\System32\cmd.exe /c "C:\Program Files (x86)\Git\bin\sh.exe" --login -i -c gitpushubun
+C:\Windows\System32\cmd.exe /c "C:\Program Files (x86)\Git\bin\sh.exe" --login -i -c 'ls'
