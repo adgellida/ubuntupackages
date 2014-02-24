08a2cf87b8cba548c94e81403c907af9f47d5403
Antonio David Gellida Lavara <tonigellida@gmail.com>
2014-02-24 20:23:01 +0100

    'Updating'

--- gitpushubun.bat
+++ gitpushubun.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Pushing changes to ubuntupakages
 set PATH=%PATH%;"C:\Program Files (x86)\Git\bin\"
-cd "C:\Users\Tonin\PERSONAL\PROYECTOS\github\ubuntupackages"
+cd "D:\PERSONAL\PROYECTOS\github\ubuntupackages"
 git add .
 git add -u
 git commit -m 'Updating'
